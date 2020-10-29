43bf18ad33e2bc6b7a13c0e88d6cdc45615d697a
Jeff Ashton <JeffAshton@users.noreply.github.com>
2020-10-29 18:04:44 -0400

    Writing node action

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM alpine:3.12
-
-COPY entrypoint.sh /entrypoint.sh
-
-RUN apk --update add git less openssh && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm /var/cache/apk/*
-
-ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ index.js
@@ -0,0 +1,96 @@
+const child_process = require( 'child_process' );
+const core = require('@actions/core' );
+const os = require( 'os' );
+const util = require( 'util' );
+const { write } = require( 'fs' );
+
+const execFile = util.promisify(child_process.execFile);
+
+function writeLine() {
+	process.stdout.write( os.EOL );
+}
+
+async function gitFetch( remote, ref ) {
+
+	console.info( `> git fetch --verbose ${remote} ${ref}` );
+
+	const { stdout, stderr } = await execFile( 'git', [
+		'fetch',
+		'--verbose',
+		remote,
+		ref
+	] );
+
+	if( stdout ) {
+		process.stdout.write( stdout );
+	}
+
+	if( stderr ) {
+		process.stderr.write( stderr );
+	}
+}
+
+async function gitShowRef( ref ) {
+
+	console.info( `> git show-ref --hash --verify ${ref}` );
+	
+	const { stdout, stderr } = await execFile( 'git', [ 
+		'show-ref',
+		'--hash',
+		'--verify',
+		ref
+	] );
+
+	if( stderr ) {
+		process.stderr.write( stderr );
+	}
+
+	const hash = stdout;
+	if( !hash ) {
+		throw new Error( 'git show-ref did not return a hash.' );
+	}
+
+	return hash;
+}
+
+async function run() {
+	try {
+		const remote = core.getInput( 'remote' );
+		const ref = core.getInput( 'ref' );
+		
+		const localRef = 'HEAD';
+		const remoteRef = `refs/remotes/${remote}/${ref}`;
+
+		writeLine();
+
+		await gitFetch( remote, ref );
+		writeLine();
+
+		const localHash = await gitShowRef( localRef );
+		core.info( `${localRef} == ${localHash}` );
+
+		const remoteHash = await gitShowRef( remoteRef );
+		core.info( `${remoteRef} == ${remoteHash}` );
+
+		if( localHash === remoteHash ) {
+			core.info( `${localRef} == ${remoteRef}` )
+
+		} else {
+			core.setFailed( `${localRef} != ${remoteRef}` )
+		}
+
+	} catch( error ) {
+
+		if( error.stdout ) {
+			process.stdout.write( error.stdout );
+		}
+
+		if( error.stderr ) {
+			process.stderr.write( error.stderr );
+		}
+
+		core.setFailed( error.message );
+	}
+}
+
+run();
